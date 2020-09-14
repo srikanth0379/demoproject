bd3b01f350d57c0b58b1bcf05994f170df000af3
srikanth0379 <srikanth0379@gmail.com>
2020-09-14 09:41:12 +0000

    Add project files.

---
+++ demoproject/dbo/Tables/sampletable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[sampletable]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(50) NULL, 
+    [age] VARCHAR(50) NULL, 
+    [phone] VARCHAR(50) NULL, 
+    [email] VARCHAR(50) NULL
+)
